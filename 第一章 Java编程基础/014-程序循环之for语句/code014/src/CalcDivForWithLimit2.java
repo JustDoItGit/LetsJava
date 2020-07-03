3af94d11b7c7c12a00642e26407ba64331b66991
cool <cool_kj@126.com>
2020-07-03 15:26:22 +0800

    修改一处错误

--- 第一章 Java编程基础/014-程序循环之for语句/code014/src/CalcDivForWithLimit2.java
+++ 第一章 Java编程基础/014-程序循环之for语句/code014/src/CalcDivForWithLimit2.java
@@ -5,11 +5,11 @@ public class CalcDivForWithLimit2 {
 
         int foundCount = 0;
         int toBeFound = 5;
-        for (int i = 0; i < 100 && toBeFound > foundCount; i++, foundCount++) {
+        for (int i = 0; i < 100 && toBeFound > foundCount; i++, dividend++) {
             if (dividend % divisor == 0) {
                 System.out.println(dividend + "可以整除" + divisor + "。商为" + dividend / divisor);
+                foundCount++
             }
-            dividend++;
         }
         System.out.println("总共找到" + foundCount + "个可以整除" + divisor + "的数。");
     }
